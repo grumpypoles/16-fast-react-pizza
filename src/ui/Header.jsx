79adfc31168be4212479a6a0be1d355c0999a6d0
grumpypoles <39036663+grumpypoles@users.noreply.github.com>
2024-05-13 11:19:28 +1200

    Implementing App Layout - children elements in the router.

---
+++ src/ui/Header.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom"
+function Header() {
+    return (
+        <div>
+            <Link to='/'> Fast React Pizza Co.</Link>
+            <p>Fred </p>
+        </div>
+    )
+}
+
+export default Header
